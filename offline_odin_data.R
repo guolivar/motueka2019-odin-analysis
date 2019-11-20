2daa2c3ba2a5d2a950ec4d878636710d0de79e23
Gustavo Olivares <1390863+guolivar@users.noreply.github.com>
2019-11-20 22:38:32 +1300

    Some work from home

--- offline_odin_data.R
+++ offline_odin_data.R
@@ -7,85 +7,124 @@
 #' author: "Gustavo Olivares"
 #' ---
 
+# Clear the environment
+rm(list = ls())
 # Libraries setup
 library(readr)
 library(openair)
-
+base_path <- path.expand('~/data/Motueka/ODIN/')
 # Paths
-datapath <- path.expand('~/data/Motueka/ODIN/00003/')
-datafile <- 'DATA.TXT'
+folders_to_work <- read.delim(paste0(base_path,'dirs'),
+                              header = FALSE)
+#for (c_dir in as.array(folders_to_work$V1)){
+  c_dir <- '00153/'
+  datapath <- paste0(base_path,c_dir)
+  print(datapath)
+  datafile <- 'DATA.TXT'
+  
+  # Read data file
+  if (!file.exists(paste0(datapath,datafile))){
+    next
+  }
+  odin.data <- try(read_delim(paste0(datapath,datafile),
+                              skip = 1,
+                              delim = ";",
+                              col_types = 'iiiiiiicniinnnccccccc',
+                              col_names = c('framelength',
+                                            'PM1',
+                                            'PM2.5',
+                                            'PM10',
+                                            'PM1x',
+                                            'PM2.5x',
+                                            'PM10x',
+                                            'GAS1sn',
+                                            'GAS1',
+                                            'TGAS1',
+                                            'GAS2',
+                                            'Temperature',
+                                            'RH',
+                                            'DeviceID',
+                                            'Serialn',
+                                            'Day',
+                                            'Time',
+                                            'Day2',
+                                            'Time2',
+                                            'Day3',
+                                            'Time3'),
+                              escape_double = TRUE,
+                              trim_ws = TRUE))
+  if (inherits(odin.data,"try-error")){
+    next
+  }
+  
+  
+  
+  # Correct date in data
+  # Find the observations with Date2 and Date3
+  odin.data$Day[is.na(odin.data$Day)] <- ""
+  odin.data$Day2[is.na(odin.data$Day2)] <- ""
+  odin.data$Day3[is.na(odin.data$Day3)] <- ""
+  # Generate the 3 date fields
+  # Clear date entries where logging failed
+  nodate <- which(nchar(paste(odin.data$Day,odin.data$Time))!=19)
+  odin.data$Day[nodate] <- NA
+  odin.data$Time[nodate] <- NA
+  idx <- !is.na(odin.data$Day)
+  odin.data$date1[idx] <- as.POSIXct(paste(odin.data$Day[idx],odin.data$Time[idx]),tz='NZST')
+ 
+  odin.data$date2 <- odin.data$date1
+  nodate <- which(nchar(paste(odin.data$Day2,odin.data$Time2))!=19)
+  odin.data$Day2[nodate] <- NA
+  odin.data$Time2[nodate] <- NA
+  odin.data$Day2[is.na(odin.data$Day2)] <- ""
+  indx <- (odin.data$Day2>"2019")
+  
+  odin.data$date3 <- as.POSIXct(paste(odin.data$Day3,odin.data$Time3),tz='NZST')
+  nodate <- which(nchar(paste(odin.data$Day3,odin.data$Time3))!=19)
+  odin.data$Day3[nodate] <- NA
+  odin.data$Time3[nodate] <- NA
+  odin.data$Day3[is.na(odin.data$Day3)] <- ""
+  indx <- (odin.data$Day3>"2019")
+  
+  odin.data$date3 <- odin.data$date1
+  odin.data$date3[indx] <- as.POSIXct(paste(odin.data$Day3[indx],odin.data$Time3[indx]),tz='NZST')
+  
+  
+  # Find the first record with "correct" data
+  first_date <- which(odin.data$Day>"2019")[1]-1
+  if (first_date>0){
+    # Use this observation to correct the clock in ALL date1 clock not set records
+    timediff <- odin.data$date2[first_date] - odin.data$date3[first_date]
+    to_correct <- (odin.data$Day < "2019") & (odin.data$Day2 < "2019")
+    odin.data$date1[to_correct] <- odin.data$date1[to_correct] + timediff
+  }
+  
+  # Fix reboots with lost power
+  to_correct <- odin.data$date1 < as.POSIXct("2019-05-01 00:00:00",tz='NZST')
+  odin.data$date1[to_correct] <- odin.data$date1[to_correct] + (odin.data$date2[to_correct] - odin.data$date3[to_correct])
+  
+  # Use READING date for timestamp
+  odin.data$date <- odin.data$date1
+  # remove data without correct date
+  odin.data <- subset(odin.data, date > as.POSIXct('2019-01-01 00:00:00', tz= "NZST"))
+  
+  # Prepare data to be merged with telemetered data
+  # 10 minutes average starting on "2019-04-25 00:00:00 NZST"
+  odin.data.10min <- timeAverage(odin.data,avg.time = '10 min',data.thresh = 0.75,start.date = "2019-04-25 00:00:00")
+  odin.data.10min$Serialn <- odin.data$Serialn[1]
+  # Remove extra columns
+  odin.data.10min$framelength <- NULL
+  odin.data.10min$date1 <- NULL
+  odin.data.10min$date2 <- NULL
+  odin.data.10min$date3 <- NULL
+  odin.data.10min$PM1x <- NULL
+  odin.data.10min$PM10x <- NULL
+  odin.data.10min$PM2.5x <- NULL
+  # Add "COARSE" data
+  odin.data.10min$PMc <- odin.data.10min$PM10 - odin.data.10min$PM2.5
+  
+  write_csv(odin.data.10min,paste0(datapath,'data_10min.txt'))
 
-# Read data file
-odin.data <- read.delim(paste0(datapath,datafile),
-                        header = FALSE,
-                        skip = 1,
-                        sep = ";",
-                        colClasses = c('integer',
-                                       'integer',
-                                       'integer',
-                                       'integer',
-                                       'integer',
-                                       'integer',
-                                       'integer',
-                                       'character',
-                                       'numeric',
-                                       'integer',
-                                       'integer',
-                                       'numeric',
-                                       'numeric',
-                                       'numeric',
-                                       'character',
-                                       'character',
-                                       'character',
-                                       'character',
-                                       'character',
-                                       'character',
-                                       'character'),
-                        col.names = c('framelength',
-                                      'PM1',
-                                      'PM2.5',
-                                      'PM10',
-                                      'PM1x',
-                                      'PM2.5x',
-                                      'PM10x',
-                                      'GAS1sn',
-                                      'GAS1',
-                                      'TGAS1',
-                                      'GAS2',
-                                      'Temperature',
-                                      'RH',
-                                      'DeviceID',
-                                      'Serialn',
-                                      'Day',
-                                      'Time',
-                                      'Day2',
-                                      'Time2',
-                                      'Day3',
-                                      'Time3'))
-
-
-
-# Correct date in data
-timediff <- start_timestamp - as.POSIXct(paste(odin.data$Day,odin.data$Time)[1],
-                                         format = '%d/%m/%Y %H:%M:%S', tz = 'PST')
-odin.data$date <- as.POSIXct(paste(odin.data$Day,odin.data$Time),
-                             format = '%d/%m/%Y %H:%M:%S', tz = 'PST') + timediff
-
-# Plot timeseries
-timePlot(odin.data,pollutant = c('PM1','PM2.5','PM10'),
-         group = TRUE,
-         avg.time = '10 min',
-         y.relation = 'free',
-         main = odin.data$Serialn[1])
-
-timeVariation(odin.data,pollutant = c('PM1','PM2.5','PM10'),
-              main = odin.data$Serialn[1])
-
-timePlot(odin.data,pollutant = c('Temperature','RH'),
-         group = FALSE,
-         avg.time = '1 hour',
-         y.relation = 'free',
-         main = odin.data$Serialn[1])
-
-timeVariation(odin.data,pollutant = c('Temperature','RH'),
-              main = odin.data$Serialn[1])
+  
+  
+  
